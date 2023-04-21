85e2bef375b8bfc1bbe0a765ab98139b0bf4f33c
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2023-04-21 09:23:59 +0300

    test pull home on work

--- main/java/com/levochka108/oop/program/Program.java
+++ main/java/com/levochka108/oop/program/Program.java
@@ -1,17 +1,8 @@
-package java.com.levochka108.oop.program;
-
-import com.levochka108.oop.es1.Student;
-
-import java.util.List;
-
-import static com.levochka108.privat.ex.ArraysOfString.program;
-import static com.levochka108.privat.ex.MultiMatrix.multiMatrix;
-
+package main.java.com.levochka108.oop.program;
 public class Program {
     public static void main(String[] args) {
 
-        program(args);
-        multiMatrix();
+
 
     }
 
