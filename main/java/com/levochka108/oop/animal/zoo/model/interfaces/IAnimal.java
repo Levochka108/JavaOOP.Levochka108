ec8fe7493dc9eca8ddbb3d90c68a08dacb4e49f7
Dimitrii Klochkov <101431127+Levochka108@users.noreply.github.com>
2023-04-30 15:30:22 +0300

    Update IAnimal.java

--- main/java/com/levochka108/oop/animal/zoo/model/interfaces/IAnimal.java
+++ main/java/com/levochka108/oop/animal/zoo/model/interfaces/IAnimal.java
@@ -1,11 +1,5 @@
 package com.levochka108.oop.animal.zoo.model.interfaces;
 
-/**
- * На языке Java реализовать интерфейс "Животное" со следующими полями:
- * 1.Рост животного
- * 2.Вес животного
- * 3.Цвет глаз животного
- */
 public interface IAnimal {
     double getHeight();
     double getWeight();
